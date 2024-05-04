39938efe1274e021213ead2e5cfb5016cd54c841
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-04 19:57:11 +0530

    added

---
+++ countOccurence.js
@@ -0,0 +1,17 @@
+let str = "abchsdkjskjfeijffiejieorieomxcnxzcmdcndc";
+
+function countOccurence(str) {
+  let obj = {};
+
+  for (let item of str) {
+    if (obj[item]) {
+      obj[item]++;
+    } else {
+      obj[item] = 1;
+    }
+  }
+  console.log(obj);
+  return obj;
+}
+
+console.log(countOccurence(str));

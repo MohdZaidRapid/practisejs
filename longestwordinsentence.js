dc59eb043218b3180aeb2b6553e6791eacf7e390
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-06 23:14:43 +0530

    addwed

---
+++ longestwordinsentence.js
@@ -0,0 +1,19 @@
+let str =
+  "hello nameeeee my name is zaid jdksjdkjskjdkjskjdkskjdkjskjdkjskjdkjsd skjdksjkdjskjdkjskdkjskdjks hahahahahhahah   pokeonoernoonononotnonrotnornntooioioimoioxuodunowue ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp";
+
+function longestWordInSentence(str) {
+  let word = str.split(" ");
+  let max = 0;
+  let str1 = "";
+  //   console.log(word);
+  word.map((w) => {
+    if (w.length >= max) {
+      //   max = w.length;
+      str1 = w;
+    }
+  });
+  console.log(str1);
+  return max;
+}
+
+longestWordInSentence(str);

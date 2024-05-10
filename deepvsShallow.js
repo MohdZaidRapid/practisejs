71e31d623c599a20e5322f016153758f065b8c7d
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-10 16:08:02 +0530

    aded

--- deepvsShallow.js
+++ deepvsShallow.js
@@ -8,13 +8,21 @@ const original = {
 };
 
 // shallow copy
-const obj1 = { ...original };
+// const obj1 = { ...original };
 
-obj1.name = "zaid";
-obj1.address.country = "delhi";
-console.log(obj1);
+// obj1.name = "zaid";
+// obj1.age = 3000;
+// obj1.address.country = "delhi";
+// console.log(obj1);
+// console.log(original);
 
 ///////////////////////////////////////////////////////////////
 // deep copy
 
+const obj2 = JSON.parse(JSON.stringify(original));
 
+// const obj = obj2.address.country;
+obj2.address.country = "free";
+
+console.log(obj2);
+console.log(original);

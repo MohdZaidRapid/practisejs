fd2c5708ac4783d270f1159a356b3c0532f7deb7
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-05-11 23:01:06 +0530

    added

---
+++ callApplyBind.js
@@ -0,0 +1,18 @@
+let person1 = {
+  name: "Adam AHS",
+  age: 25,
+
+  showDetails: function () {
+    console.log(`${this.name} is  ${this.age} years old`);
+  },
+};
+
+let person2 = {
+  name: "Steve",
+  age: 20,
+  showDetails: function () {
+    console.log(`${this.name} is  ${this.age} years old`);
+  },
+};
+
+person1.showDetails();
--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,17 +1,3 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 9];
-
-function removeDuplicate(arr) {
-  let arr2 = [];
-  arr.map((item, index) => {
-    // console.log(item);
-    if (item === index) {
-      console.log(item);
-      arr2.push(item);
-    }
-    // console.log(arr2);
-    return arr2;
-  });
-}
-
-// console.log(removeDuplicate(arr));
-removeDuplicate(arr);
+let arr = [1, 2, 3, 4, 4, 5, 6, 6];
+let uniqueArr = arr.filter((item, index) => arr.indexOf(item) === index);
+console.log(uniqueArr); // [1, 2, 3, 4, 5, 6]

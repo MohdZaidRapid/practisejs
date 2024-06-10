f5ae897c45bd5e43ed6df229b221c162dccfd069
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-06-10 15:21:29 +0530

    added

--- countingSort.cpp
+++ countingSort.cpp
@@ -16,12 +16,12 @@ void countSort(int arr[],int n){
     for(int i=1;i<=k;i++){
         count[i]+=count[i-1];
     }
-
+// jo
     int output[n];
     for(int i=n-1;i>=0;i--){
         output[--count[arr[i]]]=arr[i];
     }
-
+// 
     for(int i=0;i<n;i++){
         arr[i]=output[i];
     }
